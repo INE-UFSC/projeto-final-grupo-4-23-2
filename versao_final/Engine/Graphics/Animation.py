6511e0384d168107b8009f57c944a271c14cbe98
Anderson Simioni <anderson.simioni22@gmail.com>
2023-11-16 15:11:06 -0300

    Animation fix

--- versao_final/Engine/Graphics/Animation.py
+++ versao_final/Engine/Graphics/Animation.py
@@ -3,13 +3,14 @@ import pygame
 from Engine.Graphics.IGraphicsApi import IGraphicsApi
 
 class Animation:
-    def __init__(self, img_name, duration:int, sprite_size:int, speed:float=1):
+    def __init__(self, img, loop=-1, speed:float=1):
         self.__time = 0.0
-        self.__duration = duration
-        self.__img_name = img_name
+        self.__img = img
         self.__speed = speed
-        self.__sprite_size = sprite_size
-        self.__img = pygame.image.load(self.__img_name).convert()
+        w, h = img.get_size()
+        self.__sprite_size = h
+        self.__duration = w//h
+        self.__end_animation = False
         
     def play(self, delta_time:float):
         self.__time += delta_time*self.__speed
