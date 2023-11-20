5a01fe6409e2d7eb1cd327918b6a119600040c62
Victória Rodrigues Veloso <victoria.rvv@outlook.com>
2023-11-19 22:23:56 -0300

    adicionando end maze flag e ajustando interface

--- prototipo/GameObject/teste.py
+++ prototipo/GameObject/teste.py
@@ -1,38 +1,9 @@
-from Player import Player
 import pygame
 
-largura = 640
-altura = 480
+pygame.init()
 
-PRETO = (0, 0, 0)
+# Obtém a lista de fontes instaladas
+font_list = pygame.font.get_fonts()
 
-tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption('Sprites')
-
-player = Player((4, 4), "victória", 100, 5, block=None)
-relogio = pygame.time.Clock()
-
-executando = True
-
-while executando:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            executando = False
-
-    keys = pygame.key.get_pressed()  # Captura o estado das teclas
-    
-    if keys[pygame.K_UP]:
-        player.move(pygame.K_UP)
-    if keys[pygame.K_DOWN]:
-        player.move(pygame.K_DOWN)
-    if keys[pygame.K_LEFT]:
-        player.move(pygame.K_LEFT)
-    if keys[pygame.K_RIGHT]:
-        player.move(pygame.K_RIGHT)
-        
-    tela.fill(PRETO)
-    player.draw_raccon(tela)
-    pygame.display.flip()
-    relogio.tick(30)
-
-pygame.quit()
+for font in font_list:
+    print(font)
--- versao_final/MazeGame/Objects/EndMazeFlag.py
+++ versao_final/MazeGame/Objects/EndMazeFlag.py
@@ -1,8 +1,26 @@
 import math
 from Engine.Structs.GameObject import GameObject
-from versao_final.Engine.Physics.CollisionPolygon import CollisionPolygon
-from versao_final.Engine.Structs.Vector3 import Vector3
+from Engine.Graphics.IGraphicsApi import IGraphicsApi
+from Engine.Physics.CollisionPolygon import CollisionPolygon
+from Engine.Structs.Vector3 import Vector3
+from Engine.Structs.ResourceManager import ResourceManager
+from Engine.Graphics.Animation import Animation
 
 class EndMazeFlag(GameObject):
-    def __init__(self, initial_position: Vector3 = ..., initial_rotation_axis: Vector3 = ..., initial_speed: float = 0, initial_acceleration: float = 0, break_cof: float = 0, max_speed=math.inf, collision_polygons: [CollisionPolygon] = ...):
-        super().__init__(initial_position, initial_rotation_axis, initial_speed, initial_acceleration, break_cof, max_speed, collision_polygons)
+    def __init__(self, flag_size):
+        super().__init__()
+
+        self.__resource_manager = ResourceManager()
+        self.__animation = Animation(self.__resource_manager.get_image("flag_red.png"), speed=20)
+
+
+    def handle_on_collision(self):
+        pass
+
+    def render_graphics(self, graphics_api: IGraphicsApi):
+        return super().render_graphics(graphics_api)
+        self.__animation.render(graphics_api,self.get_position().get_x(), self.get_position().get_y())
+
+    def loop(self):
+        self.__animation.play(self.get_world().get_delta_time())
+
