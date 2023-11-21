806c831487eaae157b583aac9fe888c0e8fee3bd
Anderson Simioni <anderson.simioni22@gmail.com>
2023-11-21 10:57:44 -0300

    Fix

--- versao_final/Engine/Structs/GameObject.py
+++ versao_final/Engine/Structs/GameObject.py
@@ -106,8 +106,8 @@ class GameObject(PhysicsObject, GraphicsObject, ABC):
 
             if len(collisions)>0:
                 self.handle_on_collision(collisions)
-                others = {}
-                for obj in 
+                #others = {}
+                #for obj in 
         else: 
             self.get_position().transform_2d(tr_len, self.get_rotation_axis())
             
