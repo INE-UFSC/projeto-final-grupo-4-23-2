8070c838099c3ba7695795414fe71d1147403758
Felipe Pierotti <felipemacacari@gmail.com>
2023-11-22 19:19:06 -0300

    ranking logic

---
+++ versao_final/MazeGame/Ranking/PlayerData.py
@@ -0,0 +1,35 @@
+class GameModeEnum(Enum):
+  EASY=1,
+  NORMAL=2,
+  HARD=3
+
+class PlayerData:
+  # best_time: int (tempo em segundos ou milisegundos) PRECISA DEFINIR
+  def __init__(self, name):
+    self.__name = name
+    self.__results = {} # [mode] -> best_time
+       
+    # self.__mode = mode
+    # self.__best_time = best_time
+    
+  @property
+  def name(self):
+    return name
+  
+  # @property
+  # def mode(self):
+  #   return mode
+  
+  # @property
+  # def best_time(self):
+  #   return best_time
+  
+  @property 
+  def results(self):
+    return self.__results
+
+  def get_result(self, mode):
+    return self.__results.get(mode)
+  
+  def update_time(self, mode, time):
+    self.__results[mode] = time
---
+++ versao_final/MazeGame/Ranking/RankingManager.py
@@ -0,0 +1,44 @@
+from MazeGame.Ranking.DAO import DAO
+
+class Singleton(type):
+  _instances = {}
+
+  def __call__(cls, *args, **kwargs):
+    if cls not in cls._instances:
+      instance = super().__call__(*args, **kwargs)
+      cls._instances[cls] = instance
+    return cls._instances[cls]
+
+class RankingManager(metaclass=Singleton):
+  def __new__(cls, *args, **kwargs):
+    if not cls._instance:
+      cls._instance = super().__new__(cls, *args, **kwargs)
+        # Inicialização de instância aqui, se necessário.
+    return cls._instance
+    
+  def __init__(self):
+    self.__dao = DAO()
+    
+  def get_results_by_mode(self, mode):
+    results = self.__dao.get_all_players_data()
+    
+    results_mode = {}
+    
+    for r in results:
+      time = r.get_result(mode)
+      if time:
+        results_mode[r.name] = time ## Seria ideal criar uma DTO para esse tipo de informação
+        
+    return results_mode
+  
+  def get_results_by_player(self, name):
+    player_data = self.__dao.get_player_data(name)
+    
+    if player_data:
+      return player_data.results
+    
+  def set_player_new_time(self, name, mode, time):
+    player_data = self.__dao.get_player_data(name)
+    
+    if player_data and player_data.get_result(mode) > time:
+      player_data.update_time(mode, time)
