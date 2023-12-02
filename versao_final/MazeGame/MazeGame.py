d294c978e24d2b710b4dd5e1ba62df4440b5f52d
Felipe Pierotti <felipemacacari@gmail.com>
2023-12-01 22:07:52 -0300

    ajustando a mazegame

--- versao_final/MazeGame/MazeGame.py
+++ versao_final/MazeGame/MazeGame.py
@@ -27,11 +27,22 @@ class MazeGame(Game):
         
         self.__count_actors = 0
         
-        # terrain = RandomTerrain("TX Tileset Grass.png", 16, block_size=block_size, size=s, scale=2)
-        # terrain.create_random_terrain()
-        # terrain.set_position(Vector3(self.__iw, self.__ih, -9999))
-        # self.get_world().add_object(terrain)
-        
+        self.create_maze_map()
+            
+    def loop(self, event=None):
+        time_at = time.time()
+        self.__current_duration = time_at - self.__start_time
+
+        # print(rand)
+        if int(time_at - self.__last_power_up) >= 5:
+            self.__last_power_up = time_at
+            rand = random.randint(5, 10)
+            if rand % 2 != 0:
+                self.generate_random_power_up()
+            else:
+                self.generate_random_obstacle()
+                
+    def create_maze_map(self):
         mazeMap = Maze(size=s, block_size=block_size)
         mazeMap.set_position(Vector3(self.__iw, self.__ih, 0))
         #mazeMap.set_render_collisions_polygons(True)
@@ -48,33 +59,17 @@ class MazeGame(Game):
         flag = EndMazeFlag(initial_position=Vector3(end_pos('x'), end_pos('y'), 0), collision_polygons=[Square(size=15)])
         flag.set_render_collisions_polygons(True)
         self.get_world().add_object(flag)
-
-    
-    def loop(self, event=None):
-        time_at = time.time()
-        self.__current_duration = time_at - self.__start_time
-
-        rand = random.randint(5, 10)
-        if int(time_at - self.__last_power_up) >= rand:
-            self.__last_power_up = time_at
-            if rand % 2 != 0:
-                self.generate_random_power_up()
-            else:
-                self.generate_random_obstacle()
         
     def generate_random_power_up(self):
         PowerUp = random.choice([PowerUpLife, PowerUpSpeed])
         random_pos = self.__maze.get_random_free_position(margin=Vector3(self.__iw, self.__ih))
-        power_up = PowerUp(initial_position=random_pos,collision_polygons=[Square(self.settings.get_block_size()*2)])
-        power_up.set_render_collisions_polygons(True)
+        power_up = PowerUp(initial_position=random_pos,collision_polygons=[Square(self.settings.get_block_size())])
+        # power_up.set_render_collisions_polygons(True)
         self.get_world().add_object(power_up)
         
     def generate_random_obstacle(self):
         Obstacle = random.choice([ObstacleLife, ObstacleSpeed])
         random_pos = self.__maze.get_random_free_position(margin=Vector3(self.__iw, self.__ih))
-        obstacle = Obstacle(initial_position=random_pos,collision_polygons=[Square(self.settings.get_block_size()*2)])
-        obstacle.set_render_collisions_polygons(True)
+        obstacle = Obstacle(initial_position=random_pos,collision_polygons=[Square(self.settings.get_block_size())])
+        # obstacle.set_render_collisions_polygons(True)
         self.get_world().add_object(obstacle)
-        
-    
-          
