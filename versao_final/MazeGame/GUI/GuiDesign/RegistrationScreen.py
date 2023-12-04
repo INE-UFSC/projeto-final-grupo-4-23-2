504d218c2a7b8d6ed8b8b5342519a5e1c676ed68
Anderson Simioni <anderson.simioni22@gmail.com>
2023-12-04 20:02:15 -0300

    Fix reg window type text

--- versao_final/MazeGame/GUI/GuiDesign/RegistrationScreen.py
+++ versao_final/MazeGame/GUI/GuiDesign/RegistrationScreen.py
@@ -9,8 +9,12 @@ class RestrationScreen(ScreenBase):
         self.__label_surface = None
         self.__color = None
         self.__input_box = None
+        self.__typed_name = "Joao"
+        self.__font = None
         self.initialize_screen()
 
+    def get_typed_name(self):
+        return self.__typed_name
 
     def screen_design(self):
 
@@ -19,9 +23,9 @@ class RestrationScreen(ScreenBase):
         self.background_image = pygame.transform.scale(self.background_image, (self.width, self.height))
         
         #Textos
-        font = pygame.font.SysFont('comicsansms', size=30)
-        self.text_surface = font.render("Insira um nome para o seu jogador ou", True, (255, 255, 255))
-        self.__text2_surface = font.render("o nome já cadastrado", True, (255, 255, 255))
+        self.__font = pygame.font.SysFont('comicsansms', size=30)
+        self.text_surface = self.__font.render("Insira um nome para o seu jogador ou", True, (255, 255, 255))
+        self.__text2_surface = self.__font.render("o nome já cadastrado", True, (255, 255, 255))
         self.text_rect = self.text_surface.get_rect(center=((self.width//3)+100, 60))
         self.__text2_rect = self.__text2_surface.get_rect(center=((self.width//3)+100, 90))    
         
@@ -29,22 +33,31 @@ class RestrationScreen(ScreenBase):
         button_info = [("  Iniciar partida  ", self.width-110,( self.height//3)+50 ),("       Cadastrar      ", self.width-110, ( self.height//3)+150), ("Consultar cadastros", self.width-110, ( self.height//3)+250)]
         images_buttons = ["button_red.png", "button_red.png", "button_red.png"]
         self.create_buttons(button_info, images=images_buttons, size_button=self.width//40)
+        
 ##########caixa de texto
         label_text = "Nome: "
-        self.__label_surface = font.render(label_text,True, (255,255,255))
+        self.__label_surface = self.__font.render(label_text,True, (255,255,255))
         self.__input_box = pygame.Rect((self.width-(self.width-120), ( self.height//3)+100 ,(self.width//2)-70,35))
         self.__color = pygame.Color('white')
     
     def render(self):
-
+        keys = "qwertyuiopasdfghjklzxcvbnm0123456789"
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if str(event.unicode).lower() in " qwertyuiopasdfghjklzxcvbnm0123456789\x08":
+                    if event.unicode == "\x08": self.__typed_name = self.__typed_name[:-1]
+                    elif len(self.__typed_name) < 12: self.__typed_name += event.unicode
+                    
         self.screen.blit(self.background_image, (0, 0))
         self.screen.blit(self.text_surface, self.text_rect)
         self.screen.blit(self.__text2_surface, self.__text2_rect)
         self.screen.blit(self.__label_surface, (self.width-(self.width-20),( self.height//3)+100))
+        
         pygame.draw.rect(self.screen, self.__color, self.__input_box)
-
+        self.screen.blit(self.__font.render(self.__typed_name, True, (0,0,0)), (125, ( self.height//3)+100) )
+        
 
         for key, button in self.buttons.items():
             button.change_color(pygame.mouse.get_pos())
-
             button.update(self.screen)
